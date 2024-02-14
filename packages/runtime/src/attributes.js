83457a371a9f71fae5062916e86db5852b395893
Geoff Jamieson <geoff.ijamieson@gmail.com>
2024-02-14 14:28:21 -0500

    added attributes and events files

---
+++ packages/runtime/src/attributes.js
@@ -0,0 +1,105 @@
+/**
+ *
+ * Sets the attributes of an element
+ *
+ * Note: It does NOT remove attributes that are not present in the new attributes, except in the case of the `class` attribute.
+ *
+ * @param {HTMLElement} el element to add attributes to
+ * @param {object} attributes attributes to set
+ */
+export function setAttributes(el, attributes) {
+  const { class: className, style, ...otherAttributes } = attributes;
+
+  // set class
+  if (className) {
+    setClass(el, className);
+  }
+
+  // set style
+  if (style) {
+    Object.entries(style).forEach(([propName, value]) => {
+      setStyle(el, propName, value);
+    });
+  }
+
+  // set other attributes
+  for (const [key, value] of Object.entries(otherAttributes)) {
+    setAttribute(el, key, value);
+  }
+}
+
+/**
+ *
+ * @param {HTMLElement} el The element to add the class name to
+ * @param {(string | array)} className
+ */
+export function setClass(el, className) {
+  el.className = "";
+
+  // handle if className passed in as string
+  if (typeof className === "string") {
+    el.className = className;
+  }
+
+  // handle if className passed in as an array
+  if (Array.isArray(className)) {
+    el.classList.add(...className);
+  }
+}
+
+/**
+ * Sets the style on the element
+ *
+ * @param {HTMLElement} el The element to add the style to
+ * @param {string} name The name of the style property
+ * @param {string} value The value given to the style property
+ */
+export function setStyle(el, name, value) {
+  el.style[name] = value;
+}
+
+/**
+ * Removes a style from an element
+ *
+ * @param {HTMLElement} el The element to remove the style from
+ * @param {string} name The name of the style property
+ */
+export function removeStyle(el, name) {
+  el.style[name] = null;
+}
+
+/**
+ * Sets the attribute on the element.
+ *
+ * @param {Element} el The element to add the attribute to
+ * @param {string} name The name of the attribute
+ * @param {(string|number|null)} value The value of the attribute
+ */
+export function setAttribute(el, name, value) {
+  // remove attribute if null
+  if (value === null) {
+    removeAttribute(el, name);
+  } else if (name.startsWith("data-")) {
+    el.setAttribute(name, value);
+  } else {
+    el[name] = value;
+  }
+}
+
+/**
+ * Removes the attribute from the element.
+ *
+ * @param {Element} el the element where the attribute is set
+ * @param {string} name name of the attribute
+ */
+export function removeAttribute(el, name) {
+  try {
+    el[name] = null;
+  } catch (error) {
+    // Setting 'size' to null on an <input> throws an error.
+    // Removing the attribute instead works. (Done below.)
+    console.warn(`Failed to set "${name}" to null on ${el.tagName}`);
+  }
+
+  el.removeAttribute(name);
+}
---
+++ packages/runtime/src/events.js
@@ -0,0 +1,36 @@
+/**
+ *
+ * Add event listeners to an event target and returns an object containing the added listeners.
+ *
+ * @param {object} listeners The event listeners to add
+ * @param {EventTarget} el The element to add the listeners to
+ * @returns The added listeners
+ *
+ * Note: The `addEventListener()` method is available because an element node is an instance of the `EventTarget` interace.
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/EventTarget
+ */
+export function addEventListeners(listeners = {}, el) {
+  const addedListeners = {};
+
+  Object.entries(listeners).forEach(([eventName, handler]) => {
+    const listener = addEventListener(eventName, handler, el); // create event listener
+    addEventListener[eventName] = listener; // add event listener by key/value pair to our listeners obj
+  });
+
+  return addEventListener;
+}
+
+/**
+ *
+ * Adds an event listener to an event target and returns the listener
+ *
+ * @param {string} eventName the name of the event to listen to
+ * @param {(event: Event) => void} handler the event handler function
+ * @param {EventTarget} el the element to add the event listener to
+ * @returns {(event: Event) => void} the event handler function
+ */
+export function addEventListener(eventName, handler, el) {
+  el.addEventListener(eventName, handler);
+  return handler;
+}
