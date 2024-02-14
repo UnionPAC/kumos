25261632c046f3c4ff2f4baec59a07ff0b1aa0af
Geoff Jamieson <geoff.ijamieson@gmail.com>
2024-02-14 14:28:48 -0500

    updated mountDOM and added tests

--- packages/runtime/src/mount-dom.js
+++ packages/runtime/src/mount-dom.js
@@ -1,5 +1,14 @@
+import { setAttributes } from "./attributes";
+import { addEventListeners } from "./events";
 import { DOM_TYPES } from "./h";
 
+/**
+ * Creates the DOM nodes for a virtual DOM tree, mounts them in the DOM and modifies the virtual
+ * DOM tree to include the corresponding DOM nodes and event listeners.
+ *
+ * @param {object} vdom the virtual DOM node to mount
+ * @param {HTMLElement} parentEl the parent element to mount the virtual DOM node to
+ */
 export function mountDOM(vdom, parentEl) {
   // Different types of virtual nodes require different DOM nodes to be created
   switch (vdom.type) {
@@ -17,14 +26,72 @@ export function mountDOM(vdom, parentEl) {
   }
 }
 
+/**
+ *
+ * Creates the text node for a virtual DOM text node.
+ * The created 'Text' is added to the 'el' property of the virtual DOM.
+ *
+ * @param {object} vdom the virtual DOM node of type 'text'
+ * @param {HTMLElement} parentEl the parent element to mount the virtual DOM node to
+ */
 function createTextNode(vdom, parentEl) {
   const { value } = vdom;
 
   const textNode = document.createTextNode(value);
-  vdom.el = textNode; // Save a reference of the node
+  vdom.el = textNode; // Save a reference of the node to the vdom in the 'el' property
   parentEl.append(textNode); // Append to the parent element
 }
 
-function createElementNode(vdom, parentEl) {}
+/**
+ * Put function description here
+ *
+ * @param {object} vdom
+ * @param {HTMLElement} parentEl
+ */
+function createElementNode(vdom, parentEl) {
+  const { tag, children, props } = vdom;
+  // Create the element node
+  const element = document.createElement(tag);
 
-function createFragmentNodes(vdom, parentEl) {}
+  // Add the attributes and event listeners
+  addProps(element, props, vdom);
+
+  // Save a reference of the node to the vdom's 'el' property
+  vdom.el = element;
+
+  // Mount the children recursively into the element node
+  children.forEach((child) => {
+    mountDOM(child, element);
+  });
+  parentEl.append(element);
+}
+
+/**
+ *
+ * @param {HTMLElement} el
+ * @param {object} props
+ * @param {object} vdom
+ */
+function addProps(el, props, vdom) {
+  const { on: events, ...attributes } = props;
+  vdom.listeners = addEventListeners(events, el);
+
+  setAttributes(el, attributes);
+}
+
+/**
+ * Creates the fragment for a virtual DOM fragment node and its children recursively.
+ * The vdom's 'el' property is set to the parentEl (HTML Element) because a fragment doesn't actually
+ * get attached to the DOM, they are just and array of children, which are appended to the DOM.
+ *
+ * @param {object} vdom the virtual DOM node of type 'fragment'
+ * @param {HTMLElement} parentEl the parent element to mount the virtual DOM node to
+ */
+function createFragmentNodes(vdom, parentEl) {
+  const { children } = vdom;
+  vdom.el = parentEl;
+
+  children.forEach((child) => {
+    mountDOM(child, parentEl);
+  });
+}
